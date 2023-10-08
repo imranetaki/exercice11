4a2ef302d99ef7a16d4fbe3c4f689e1d8dc19587
imranetaki <146876470+imranetaki@users.noreply.github.com>
2023-10-08 22:17:41 +0200

    Add files via upload

---
+++ ex11signe.py
@@ -0,0 +1,8 @@
+x=float(input("entrer un réel svp :"))
+y=float(input("entrer un autre réel svp :"))
+if x*y>0 :
+    print("le produit des 2 réels est positfs")
+else :
+    print("le produit des 2 réels est négatif")
+    
+
